61d00ec2e979b826e1421e72882be0405e6ca058
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-05-03 00:48:36 +0300

    this is blog

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Into My Blog</h1>
+
+    <article>
+        <h2>Post 1 Blog Title</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
+
+    <article>
+        <h2>Post 2 Blog Title</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
+
+    <article>
+        <h2>Post 3 Blog Title</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
+</body>
+</html>
