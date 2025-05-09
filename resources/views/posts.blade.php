17abc8a97a0693f573b00d695e8bb92cc80d6d8f
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-05-09 15:03:17 +0300

    added data in database and get this data from model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,19 +5,29 @@
 
 <h1>Hello Into My Blog</h1>
 
-<article>
-    <a href="posts/post-1">Post 1 Blog Title</a>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
-</article>
+<section class="w-80">
+    <article>
+        <a href="posts/post-1">Post 1 Blog Title</a>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
 
-<article>
-    <a href="posts/post-2">Post 2 Blog Title</a>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
-</article>
+    <article>
+        <a href="posts/post-2">Post 2 Blog Title</a>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
 
-<article>
-    <a href="posts/post-3">Post 3 Blog Title</a>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
-</article>
+    <article>
+        <a href="posts/post-3">Post 3 Blog Title</a>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore repudiandae sapiente assumenda iure quam eum animi delectus saepe sed optio ducimus veritatis tempore, atque adipisci similique possimus architecto cupiditate. Dolorem!</p>
+    </article>
+</section>
+
+<section class="w-20">
+    <h2>Current Users</h2>
+    <ul>
+        <li>{{ $user->name }}</li>
+    </ul>
+
+</section>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
-    return view('posts');
+    $user = User::findOrFail(1);
+    return view('posts', [
+        'user' => $user
+    ]);
 });
 
 
